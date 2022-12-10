cff489b39b576d92644842d0151fe1e8c67db719
matews-sousa <mateussdamiano@gmail.com>
2022-12-10 14:06:49 -0300

    change navbar on scroll behavior

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 const links = [
@@ -20,25 +21,45 @@ const links = [
 ];
 
 const Navbar = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
   const location = useLocation();
 
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="text-white px-8 py-5 flex justify-between border-b-[0.5px] border-gray-700 z-50 fixed top-0 left-0 right-0 bg-gray-900 bg-opacity-50">
+    <nav
+      className={`text-white px-8 h-20 flex justify-between items-center border-b-[0.5px] border-gray-700 z-50 fixed top-0 left-0 right-0 transition-all bg-gray-900 ${
+        scrollPosition > 0 ? "bg-opacity-100" : "bg-opacity-75"
+      }`}
+    >
       <Link className="text-2xl font-bold" to="/">
         PopBox
       </Link>
-      <ul className="space-x-5 hidden md:flex">
+      <ul className="space-x-5 hidden md:flex h-full">
         {links.map((link) => (
-          <Link
-            className={`font-semibold h-full hover:text-white ${
+          <li
+            key={link.href}
+            className={`h-full flex items-center transition-all ${
               location.pathname === link.href
                 ? "text-white border-b-2 border-red-400"
-                : "text-gray-200"
+                : "text-gray-400"
             }`}
-            to={link.href}
           >
-            {link.text}
-          </Link>
+            <Link className={`font-semibold hover:text-white `} to={link.href}>
+              {link.text}
+            </Link>
+          </li>
         ))}
       </ul>
     </nav>

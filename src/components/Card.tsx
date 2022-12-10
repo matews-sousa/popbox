98b49a948e6d9da9d26865be52c288213f31946f
matews-sousa <mateussdamiano@gmail.com>
2022-12-10 14:09:44 -0300

    change Card to use normal img tag

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import { IMedia } from "../types/IMedia";
-import Image from "./Image";
 
 interface Props {
   data: IMedia;
@@ -10,7 +9,7 @@ interface Props {
 const Card = ({ data, type = "movie" }: Props) => {
   return (
     <Link to={`/${type}/${data.id}`} className="group">
-      <Image
+      <img
         src={`http://image.tmdb.org/t/p/w500/${data.poster_path}`}
         className="rounded-md w-full shadow-lg group-hover:scale-105 group-hover:shadow-xl transition-all duration-200"
       />
